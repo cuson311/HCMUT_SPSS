f5adf559bdee502a0e2f1f866db3f30c3e20be99
cuson311 <cuson108@gmail.com>
2023-12-03 00:24:30 +0700

    'login'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import PrintManage from "./Print_management/Print_manage";
 import History from './Historia/History'
+import Login from './Login/login';
+import SelectMember from "./Login/selectmember";
 import Header from './header&footer/Header';
 import Footer from "./header&footer/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 function App() {
   return (
     <>
       {/* <Header /> */}
-      <PrintManage />
+      <SelectMember />
       {/* <Footer /> */}
     </>
   );
